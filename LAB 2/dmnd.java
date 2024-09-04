99f269cbce269c8c5f327ade84f9e0a611d473ce
Hasanuzzaman Sojeeb <122292820+Hasanuzzaman101@users.noreply.github.com>
2024-09-04 15:27:41 +0600

    Add files via upload

---
+++ LAB 2/dmnd.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class dmnd{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter the size of the diamond: ");
+        int size = scanner.nextInt();
+
+
+        for (int i = 1; i <= size; i++) {
+
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+        for (int i = size - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
